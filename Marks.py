25f3a33c3d39c1c53ef51815b762cf578a3efbd2
killerb987 <73249112+killerb987@users.noreply.github.com>
2020-10-21 23:41:29 +0530

    Main file

---
+++ Marks.py
@@ -0,0 +1,15 @@
+marks = int(input("Enter Your Shocking Marks : "))
+if (marks >= 75):
+  print ("Wow Great Man It's A Grade. This is the best grade")
+
+elif (65 <= marks < 75):
+  print ("Chill Bro it's B Grade. This is the second best grade")
+  
+elif (50 <= marks < 65):
+  print ("Cool Bro it's C Grade. This grade significant you're in Average Position")
+  
+elif (35 <= marks < 50):
+  print ("Oh Comeon Bro it's S Grade. This grade means you're in Very Weak Position")
+  
+else:
+  print ("Holyshit You're Failed This Stream")
